5d9727f9018943ab50f7c622be720b356e51b200
Jason Kartz <jasonkartz@gmail.com>
2022-01-24 20:23:44 -0600

    added drag and drop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,106 @@
-import React from "react";
-import Container from "./Components/Container"
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import TodoItem from "./Components/TodoItem";
+import { CgAddR } from "react-icons/cg";
 
 export default function App() {
+  const [dragIndex, setdragIndex] = useState();
+  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([
+    {
+      id: nanoid(),
+      todo: "todo1",
+      completed: false,
+    },
+    {
+      id: nanoid(),
+      todo: "todo2",
+      completed: true,
+    },
+    {
+      id: nanoid(),
+      todo: "todo3",
+      completed: false,
+    },
+  ]);
+
+  const deleteTodo = (id) => {
+    const newList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(newList);
+  };
+
+  const handleCompleted = (data) => {
+    setTodoList((todos) =>
+      todos.map((el) =>
+        el.id === data.id ? { ...el, completed: !el.completed } : el
+      )
+    );
+  };
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodoList((prevData) =>
+      newTodo
+        ? [
+            ...prevData,
+            {
+              id: nanoid(),
+              todo: newTodo,
+              completed: false,
+            },
+          ]
+        : [...prevData]
+    );
+    setNewTodo("");
+  };
+
+  function handleDragStart(index) {
+    setdragIndex(index)
+    
+    
+  }
   
+  
+  function handleDragEnter(e, index) {
+    const newList = [...todoList]
+    const item = newList[dragIndex]
+    newList.splice(dragIndex, 1)
+    newList.splice(index, 0, item)
+    setdragIndex(index)
+    setTodoList(newList)
+  }
+
+  const todos = todoList.map((data, index) => (
+    <TodoItem
+      key={data.id}
+      todoItem={data.todo}
+      completed={data.completed}
+      handleCompleted={() => handleCompleted(data)}
+      deleteBtn={() => deleteTodo(data.id)}
+      handleDragStart={() => handleDragStart(index)}
+      handleDragOver={(e) => e.preventDefault}
+      handleDragEnter={(e) => handleDragEnter(e, index)}
+    />
+  ));
+
   return (
-    <Container />
+    <div className="w-1/3 m-auto mt-5 bg-amber-50 drop-shadow-lg">
+      <form
+        className="flex justify-center mb-2 bg-transparent border-b border-slate-600"
+        onSubmit={addTodo}
+      >
+        <input
+          className="w-full mr-1 bg-transparent focus:outline-none"
+          type="text"
+          value={newTodo}
+          onInput={(e) => setNewTodo(e.target.value)}
+          placeholder="New To Do"
+        ></input>
+        <button className="text-xl">
+          <CgAddR />
+        </button>
+      </form>
+      <ul className="bg-amber-50">{todos}</ul>
+    </div>
   );
 }
--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,30 +1,35 @@
 import React from "react";
-import { CgTrash, CgArrowsV } from "react-icons/cg";
+import { GoGrabber, GoTrashcan } from "react-icons/go";
 
 export default function TodoItem(props) {
   return (
     <li
       className={`flex justify-between gap-x-1.5 ${
-        props.completed && "line-through bg-stone-100"
-      } border-b border-black-600`}
+        props.completed && "line-through"
+      } border-b border-black-600 cursor-move hover:bg-stone-100`}
+      draggable
+      onDragStart={props.handleDragStart}
+      onDragEnter={props.handleDragEnter}
+      onDragLeave={props.handleDragLeave}
+      onDrop={props.handleDrop}
+      onDragOver={props.handleDragOver}
+
     >
       <div className="flex items-center gap-x-1.5 w-full">
         <input
           type="checkbox"
-          className="accent-amber-200"
-          id={props.id}
-          name={props.name}
+          className="accent-stone-300"
           defaultChecked={props.completed}
           onChange={props.handleCompleted}
         ></input>
-        <label htmlFor={props.todoItem}>{props.todoItem}</label>{" "}
+        <label htmlFor={props.todoItem} className="cursor-move">{props.todoItem}</label>
       </div>
 
       <button className="text-xl" onClick={props.deleteBtn}>
-        <CgTrash />
+        <GoTrashcan />
       </button>
-      <button className="text-xl">
-        <CgArrowsV />
+      <button className="text-xl cursor-move">
+        <GoGrabber />
       </button>
     </li>
   );
